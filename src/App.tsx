6e81ca58e73706136be662dc34f11cdf25885064
Keangkai Ruangrongwat <bosskeangkai11@gmail.com>
2022-08-10 12:04:46 +0700

    create task box and go button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
-  return (
+let name: string; //let name: any;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+// tuple
+let role: [number,string];
+// create object in typescript
+// let person: Object =>not recomment
+//' ?' in type said optional we can have it or not
+// we can have 2 type in typescript that is union '|'
+// type Person ={
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person={
+//   name:"keangkai",
+// };
+age='boss '
+
+// let printName: Function;
+// function printName(name: string){
+//   console.log(name);
+// }
+// printName("Boss")
+
+// if we don't know or we want to return more than one type we using 'any'
+// never, void, unknow
+// let printName: (name: string) => never;
+
+// Alias
+// type X ={
+//   a:string;
+//   b:number;
+// };
+// type Y ={    //if we want to using the property in type x we need to use type Y = X&
+//   c:string;
+//   d:number;
+// };
+
+// // entending type with interface
+// type Z ={
+//   g:string;
+//   h:number;
+// };
+// interface Person extends Z {
+//   name:string;
+//   age?:number;
+// };
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello World Boss
+//     </div>
+//   );
+// }
+
+const App: React.FC =() => { //FC:Function Components
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField />
     </div>
   );
-}
+};
 
 export default App;
